322d6dd3a0c2293e1c33ed84353c66ddb8191d1a
Rico Prasetya <ricoprasetya24@gmail.com>
2022-10-22 16:44:15 +0700

    menambah Tugas

--- Tugas/latihan2.py
+++ Tugas/latihan2.py
@@ -1,1 +1,11 @@
-“# latihan2”
+print("Nama : Rico Prasetya")
+print("NIM : 312210425")
+print()
+print("Hello")
+print("Saya Sedang Belajar Python")
+print()
+a=8
+b=6
+print("Variabel a=",a)
+print("Variabel b=",b)
+print("Hasil penjumlahan a+b=",a+b)
